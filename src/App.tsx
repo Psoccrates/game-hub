8b62f10066b29471a5040c713480241d02c48104
Psoccrates Stansberry <psoccratesstudio@gmail.com>
2023-08-24 10:45:54 -0400

    Fetch game genres

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { Grid, GridItem, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGrid from "./components/GameGrid";
+import GenreList from "./components/GenreList";
 
 function App() {
   return (
@@ -15,10 +16,12 @@ function App() {
       </GridItem>
       <Show above="lg">
         <GridItem area="aside">
-          <GameGrid />
+          <GenreList />
         </GridItem>
       </Show>
-      <GridItem area="main">Main</GridItem>
+      <GridItem area="main">
+        <GameGrid />
+      </GridItem>
     </Grid>
   );
 }
